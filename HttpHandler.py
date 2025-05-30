9f8d0219c623e67720e5c52e719042240d001d88
walker3354 <walker3354827.com>
2025-05-30 20:05:01 +0800

    fix some bug

--- HttpHandler.py
+++ HttpHandler.py
@@ -1,6 +1,7 @@
 from ECCOperator import ECC_P256
 import requests
 import json
+import base64
 
 
 function_list = {"send_encrypted_message":"decrypt"}
@@ -12,12 +13,18 @@ class HttpHandler:
         self.server_url = str(server_url)
         
     def send_encrypted_message(self,encrypted_message):
+        b64_encrypted_pkg = {
+            "temp_key": base64.b64encode(encrypted_message["temp_key"]).decode("utf-8"),
+            "nonce"   : base64.b64encode(encrypted_message["nonce"]).decode("utf-8"),
+            "secret"  : base64.b64encode(encrypted_message["secret"]).decode("utf-8"),
+        }
         try:
             url = self.server_url+function_list["send_encrypted_message"]
-            payload = {"encrypted_packet":encrypted_message}
+            payload = {"encrypted_packet":b64_encrypted_pkg}
             resp = requests.post(url,json=payload,timeout=time_out)
             resp.raise_for_status()
             data = resp.json()
+            print(f"server respone {data}")
         except requests.exceptions.Timeout:
             print("Error time out")
         except requests.exceptions.ConnectionError:
