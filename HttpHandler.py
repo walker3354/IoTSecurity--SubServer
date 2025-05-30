ada74abe58aeebeb510eee2fcbb834a9352c0bee
walker3354 <walker3354827.com>
2025-05-30 17:38:12 +0800

    add some http operation

--- HttpHandler.py
+++ HttpHandler.py
@@ -1,6 +1,10 @@
+from ECCOperator import ECC_P256
 import requests
+import json
+
 
 function_list = {"send_encrypted_message":"decrypt"}
+time_out = 5
 
 class HttpHandler:
 
@@ -9,7 +13,24 @@ class HttpHandler:
         
     def send_encrypted_message(self,encrypted_message):
         try:
-            url = self.server_url+function["send_encrypted_message"]
-            payload = 
-        except
+            url = self.server_url+function_list["send_encrypted_message"]
+            payload = {"encrypted_packet":encrypted_message}
+            resp = requests.post(url,json=payload,timeout=time_out)
+            resp.raise_for_status()
+            data = resp.json()
+        except requests.exceptions.Timeout:
+            print("Error time out")
+        except requests.exceptions.ConnectionError:
+            print("Connect Error")
+        except requests.exceptions.HTTPError:
+            print("Http Error")
+        except json.JSONDecodeError:
+            print("Json illegal")
         
+
+if __name__ == "__main__":
+    ecc1 = ECC_P256()
+    http_sender = HttpHandler()
+    server_pk_pem = ecc1.read_pk_pem()
+    encrypted_message = ecc1.asymmetric_encryption(b"my name is walker",server_pk_pem)
+    http_sender.send_encrypted_message(encrypted_message)
