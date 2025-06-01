40dd83ca2a5ff1be6cdf4754e9175e02c3d49d51
walker3354 <walker3354827@gmail.com>
2025-06-01 20:37:55 +0800

    add TCP handler operation

--- TCPHandler.py
+++ TCPHandler.py
@@ -1,20 +1,32 @@
 import socket
+import time
 
-HOST = '192.168.0.20'       # 空字串代表監聽所有介面（含 eth0）
-PORT = 5000     # 可自行指定未被佔用的埠號
+STM32_IP_addr = "192.168.0.20"
+STM32_port = 5000
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"TCP 伺服器啟動，監聽埠號 {PORT}")
-    conn, addr = s.accept()
-    with conn:
-        print('已連線來自', addr)
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print("連線關閉")
-                break
-            print("收到資料：", data)
-            # 若要回傳，可以：
-            # conn.sendall(b"ACK")
+class TCPHandler:
+    def __init__(self,ip_addr= STM32_IP_addr,port = STM32_port):
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket.settimeout(5.0)
+        self.connect_transmiter(ip_addr,port)
+        
+    def connect_transmiter(self,ip_addr,port):
+        try:
+            self.socket.connect((ip_addr,port))
+        except Exception as e:
+            print(f"Error while connecting to transmiter {e}")
+
+    def get_temperature(self):
+        try:
+            self.socket.sendall(b"GET_TEMP\r\n")
+            temp = self.socket.recv(64)
+            return temp.decode("utf-8", errors="ignore").strip()
+        except Exception as e:
+            print(f"Error occur while get temperature{e}\n wait 5 sec")
+            time.sleep(5)
+
+if __name__ == "__main__":
+    tcphandler = TCPHandler()
+    while True:
+        print(f"[{time.strftime('%H:%M:%S')}] 溫度：{tcphandler.get_temperature()}")
+        time.sleep(2)
