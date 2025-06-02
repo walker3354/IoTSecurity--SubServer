b761a6b1e83b6c61b9494660ea1d3948e2e4fb77
charlotte1227226 <andy20011019@gmail.com>
2025-06-02 15:25:34 +0800

    unstable ver

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+from HttpHandler import HttpHandler
 from ECCOperator import ECC_P256
+from TCPHandler import TCPHandler
 
-if __name__ == "__mian__":
-    pass
+if __name__ == "__main__":
+    TCP = TCPHandler()
+    ecc1 = ECC_P256()
+    http_sender = HttpHandler()
+    server_pk_pem = ecc1.read_pk_pem()
+    while True:
+        temp = TCP.get_temperature()
+        print(f"溫度：{temp}")
+        if temp != None:
+            encrypted_message = ecc1.asymmetric_encryption(str(temp).encode(),server_pk_pem)
+            http_sender.send_encrypted_message(encrypted_message)
